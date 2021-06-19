fa37e8e86b51bbe789584828cd1dd5a71cc3cd5a
Geunwoo Park <qkr0677@gmail.com>
2021-06-19 20:18:49 +0900

    [feat/back] urls.py_주소록 조회, 단일 주소 조회 url 분리

--- restfulapiserver/restfulapiserver/urls.py
+++ restfulapiserver/restfulapiserver/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url, include
-#addresses 폴더 내에 views.py import 시킴
+# addresses 폴더 내에 views.py import 시킴
 from addresses import views
 from django.urls import path, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
+from rest_framework.urlpatterns import format_suffix_patterns
 
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^addresses/', views.address_list),
+    path('addresses/', views.address_list),
+    path('addresses/<int:pk>', views.address_detail),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
