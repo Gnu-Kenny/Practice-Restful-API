d019a2f461538d3009ed54fef9b36f1b260657db
Geunwoo Park <qkr0677@gmail.com>
2021-06-25 03:34:50 +0900

    [feat/back] sign/ 추가

--- restfulapiserver/restfulapiserver/urls.py
+++ restfulapiserver/restfulapiserver/urls.py
@@ -1,22 +1,18 @@
 from django.conf.urls import url, include
 # addresses 폴더 내에 views.py import 시킴
 from addresses.views import address_list, address_detail
-from users.views import users, login, HelloView
-from django.urls import path, include
+from users.views import users, profile
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_simplejwt import views as jwt_views
+# from rest_framework_simplejwt import views as jwt_views
 
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('addresses/', address_list),
     path('addresses/<int:pk>', address_detail),
     path('users/', users),
-    path('login/', login),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
-         name='token_refresh'),
-    path('hello/', HelloView, name='hello'),
+    path('profile/', profile),
+    path('sign', include('account.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
