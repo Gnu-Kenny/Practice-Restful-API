79ab6c0c4b1ee3a5793e07042298e2a598ccef30
Geunwoo Park <qkr0677@gmail.com>
2021-06-25 04:08:20 +0900

    [feat/back] 비밀번호 암호화 기능 추가

--- restfulapiserver/account/views.py
+++ restfulapiserver/account/views.py
@@ -1,11 +1,12 @@
 import json
+import bcrypt                            # 암호화에 사용
+import jwt                               # 토큰 발행에 사용
 
-from django.http.response import HttpResponse
-
+from restfulapiserver.settings import SECRET_KEY  # 토큰 발행에 사용할 secret key
 from .models import Account
 
 from django.views import View
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 # from django.views.decorators.csrf import csrf_exempt
 # from django.utils.decorators import method_decorator
 
@@ -13,12 +14,23 @@ from django.http import JsonResponse
 
 
 class SignUpView(View):
-
     def post(self, request):
         data = json.loads(request.body)
         try:
             if Account.objects.filter(email=data['email']).exists():
                 return HttpResponse(status=400)
+
+            #==== 비밀번호 암호화====#
+
+            password = data['password'].encode(
+                'utf-8')                 # 입력된 패스워드를 바이트 형태로 인코딩
+            password_crypt = bcrypt.hashpw(
+                password, bcrypt.gensalt())  # 암호화된 비밀번호 생성
+            password_crypt = password_crypt.decode(
+                'utf-8')  # DB에 저장할 수 있는 유니코드 문자열 형태로 디코딩
+
+            #====================#
+
             Account(
                 email=data['email'],
                 password=data['password']
