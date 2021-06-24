4d1b4014a692500f05c414850383c7d459cc3e71
Geunwoo Park <qkr0677@gmail.com>
2021-06-25 00:20:33 +0900

    [fix/back] simplejwt 내부 view 사용

--- restfulapiserver/restfulapiserver/urls.py
+++ restfulapiserver/restfulapiserver/urls.py
@@ -1,19 +1,22 @@
 from django.conf.urls import url, include
 # addresses 폴더 내에 views.py import 시킴
 from addresses.views import address_list, address_detail
-from users import views
+from users.views import users, login, HelloView
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from rest_framework_simplejwt import views as jwt_views
+
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('addresses/', address_list),
     path('addresses/<int:pk>', address_detail),
-    path('users/', views.users),
-    path('login/', views.login),
-    url(r'^api-jwt-auth/$', obtain_jwt_token),          # JWT 토큰 획득
-    url(r'^api-jwt-auth/refresh/$', refresh_jwt_token),  # JWT 토큰 갱신
-    url(r'^api-jwt-auth/verify/$', verify_jwt_token),   # JWT 토큰 확인
+    path('users/', users),
+    path('login/', login),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('hello/', HelloView, name='hello'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
